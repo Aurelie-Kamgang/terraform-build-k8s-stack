ea0faf5e7014d921d3250ed0abdc7825b3d65cad
Nguedjang Kamgang Blondel Aurelie <74322332+Aurelie-Kamgang@users.noreply.github.com>
2025-07-29 18:52:29 +0100

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Configuration kubectl
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
